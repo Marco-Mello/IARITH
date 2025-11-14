83b9fb92ba566333691039cc7e16b55618743ea0
Marco Mello <marcoasma@insper.edu.br>
2025-11-14 13:05:29 -0300

    fibonacci rodando sem fim

--- arquivos_quartus/IARITH.vhd
+++ arquivos_quartus/IARITH.vhd
@@ -7,10 +7,16 @@ entity IARITH is
     port
     (
 		  CLOCK_50 : in STD_LOGIC;
-        INSTRUCAO	: out STD_LOGIC_VECTOR( 12 downto 0);
+		  KEY              : in  std_logic_vector(3 downto 0);
+		  LEDR             : out std_logic_vector(9 downto 0);
+        --INSTRUCAO	: out STD_LOGIC_VECTOR( 12 downto 0);
         DATA_IN : out STD_LOGIC_VECTOR(7 downto 0);
 		  DATA_OUT : out STD_LOGIC_VECTOR(7 downto 0);
-		  ADDRESS : out STD_LOGIC_VECTOR(7 downto 0)
+		  ADDRESS : out STD_LOGIC_VECTOR(7 downto 0);
+		  
+		  
+		  --TESTES
+		  HEX0, HEX1, HEX2, HEX3, HEX4, HEX5 : out STD_LOGIC_VECTOR(6 downto 0) 
     );
 	 
 end entity;
@@ -18,22 +24,49 @@ end entity;
 
 architecture comportamento of IARITH is
 
+	signal CLK_1s : std_logic;
+
 	 signal entradaPC: std_logic_vector(8 downto 0);
 	 signal saidaPC: std_logic_vector(8 downto 0);
 	 signal sig_PC_Mais1: std_logic_vector(8 downto 0);
+	 signal imedRET : std_logic_vector(8 downto 0);
 	 
+	 signal INSTRUCAO : std_logic_vector(19 downto 0);
+	 
+	 
+	 alias OpCode : std_logic_vector(3 downto 0) is INSTRUCAO(19 downto 16);
+	 alias imediato : std_logic_vector(15 downto 0) is INSTRUCAO(15 downto 0);
+	 alias ramADDR : std_logic_vector(7 downto 0) is INSTRUCAO(7 downto 0);
+	 alias imedJMP : std_logic_vector(8 downto 0) is INSTRUCAO(8 downto 0);
+	 
+	 
+	 signal PdC : std_logic_vector(10 downto 0);
+	 
+	 signal saidaDadoRAM, in_UlaA_Resultado, in_UlaB, saidaULA : std_logic_vector(15 downto 0);
+	 
+	 signal sig_flagZero, PdCIn_flagZero, sig_flagMenor, PdCIn_flagMenor : std_logic;
+	 
+	 
+	 
+	 signal sig_HEX_6CASAS : std_logic_vector(15 downto 0);
 
     begin
 	 
 	 
----FETCH
+
+	 
+	 BaseTempo : entity work.divisorGenerico
+            generic map (divisor => 2000000)   -- divide por 50M.
+            port map (clk => CLOCK_50, saida_clk => CLK_1s);
+	 
+
 		  
 		  Registrador_PC :  entity work.registradorGenerico
 		  generic map (larguraDados => 9)
         port map( DIN => entradaPC,
 		            DOUT => saidaPC,
 						ENABLE => '1',
-						CLK => CLOCK_50,
+						CLK => CLK_1s,
 						RST => '0'
 						
 						);
@@ -42,22 +75,186 @@ architecture comportamento of IARITH is
         port map( entrada => saidaPC,
         saida => sig_PC_Mais1
 		  );
-					  
-		  memoriaROM : entity work.memoriaROM
-		  generic map (dataWidth => 13, addrWidth => 9)
-        port map (Endereco => sig_PC_Mais1,
-		  Dado => INSTRUCAO
-		  );
 		  
 		  mux4x1 :  entity work.mux4x1_9Bits
         port map( entradaA_MUX => sig_PC_Mais1,
-                 entradaB_MUX =>  '0' & x"0B",
-					  entradaC_MUX => '0' & x"0C",
+                 entradaB_MUX =>  imedJMP,
+					  entradaC_MUX => imedRET,
                  entradaD_MUX =>  '0' & x"0D",
-                 seletor_MUX => "00",
+                 seletor_MUX => PdC(10 downto 9),
                  saida_MUX => entradaPC
 		   );
 			
+			
+			EndREtorno :  entity work.registradorGenerico generic map (larguraDados => 9)
+        port map( DIN => sig_PC_Mais1,
+		            DOUT => imedRET,
+						ENABLE => PdC(8),
+						CLK => CLK_1s,
+						RST => '0'
+						); 
+			
+			
+			
+					  
+		  memoriaROM : entity work.memoriaROM
+		  generic map (dataWidth => 20, addrWidth => 9)
+        port map (Endereco => saidaPC,
+		  Dado => INSTRUCAO
+		  );
+		  
+		  
+
+
+		mux2x1 :  entity work.mux2x1_16Bits
+        port map( entradaA_MUX => saidaDadoRAM,
+                 entradaB_MUX =>  imediato,
+                 seletor_MUX => PdC(7),
+                 saida_MUX => in_UlaB
+		   );
+			
+	   ACUMULADOR :  entity work.registradorGenerico generic map (larguraDados => 16)
+        port map( DIN => saidaULA,
+		            DOUT => in_UlaA_Resultado,
+						ENABLE => PdC(6),
+						CLK => CLK_1s,
+						RST => '0'
+						); 
+						
+		ULA : entity work.ULA  generic map(larguraDados => 16)
+          port map (entradaA => in_UlaA_Resultado,
+							entradaB =>  in_UlaB,
+							saida => saidaULA,
+							seletor => PdC(5 downto 4),
+							flagZero => sig_flagZero,
+							flagMenor => sig_flagMenor
+							
+			 );
+			 
+			 
+	   flagZero :  entity work.flipFlop
+        port map( DIN => sig_flagZero,
+		            DOUT => PdCIn_flagZero,
+						ENABLE => PdC(3),
+						CLK => CLK_1s,
+						RST => '0'
+						); 
+						
+						
+					 
+	   flagMenor :  entity work.flipFlop
+        port map( DIN => sig_flagMenor,
+		            DOUT => PdCIn_flagMenor,
+						ENABLE => PdC(2),
+						CLK => CLK_1s,
+						RST => '0'
+						); 
+						
+		memRAM : entity work.memoriaRAM   generic map (dataWidth => 16, addrWidth => 8)
+          port map (addr => ramADDR, we => PdC(0), re => PdC(1), habilita  => '1', dado_in => in_UlaA_Resultado, dado_out => saidaDadoRAM, clk => CLK_1s);
+						
+						
+			 
+			 
+	  PontosDeControle :  entity work.UnidadeControle
+	   port map( OpCode => OpCode,
+		            PdCIn_flagZero => PdCIn_flagMenor,
+						PdCIn_flagMenor => PdCIn_flagZero,
+						pontosDeControle => PdC
+						); 
+						
+			 
+			 
+--	   HEX_0 :  entity work.conversorHex7Seg
+--        port map(dadoHex => in_UlaA_Resultado(3 downto 0),
+--                 apaga =>  '0',
+--                 negativo => '0',
+--                 overFlow =>  '0',
+--                 saida7seg => HEX0
+--					  );
+--					  
+--		
+--					  
+--	   HEX_1 :  entity work.conversorHex7Seg
+--        port map(dadoHex => in_UlaA_Resultado(3 downto 0),
+--                 apaga =>  '0',
+--                 negativo => '0',
+--                 overFlow =>  '0',
+--                 saida7seg => HEX1);
+
+
+
+
+
+
+
+
+
+
+					  
+					  
+		REG_LEDR :  entity work.registradorGenerico generic map (larguraDados => 10)
+        port map( DIN => in_UlaA_Resultado(9 downto 0),
+		            DOUT => LEDR,
+						ENABLE => PdC(0) and
+									 ramADDR(7)	and
+									 ramADDR(6)	and
+									 ramADDR(5)	and
+									 ramADDR(4)	and
+									 ramADDR(3)	and
+									 ramADDR(2)	and
+									 ramADDR(1)	and
+									 ramADDR(0),
+						CLK => CLK_1s,
+						RST => '0'
+						); 
+					  
+					  
+	
+
+
+
+
+
+
+
+
+		REG_HEX_6CASAS :  entity work.registradorGenerico generic map (larguraDados => 16)
+        port map( DIN => in_UlaA_Resultado,
+		            DOUT => sig_HEX_6CASAS,
+						ENABLE => PdC(0) and
+									 ramADDR(7)	and
+									 ramADDR(6)	and
+									 ramADDR(5)	and
+									 ramADDR(4)	and
+									 ramADDR(3)	and
+									 ramADDR(2)	and
+									 ramADDR(1)	and
+									 ramADDR(0),
+						CLK => CLK_1s,
+						RST => '0'
+						); 
+
+	
+		
+					  
+			HEX_6CASAS : entity work.conversorHex7Seg_6casas
+    port map(
+        dadoHex        => sig_HEX_6CASAS,
+        apaga          => '0',
+
+        saida7segHEX0  => HEX0,   -- unidades
+        saida7segHEX1  => HEX1,   -- dezenas
+        saida7segHEX2  => HEX2,   -- centenas
+        saida7segHEX3  => HEX3,   -- milhares
+        saida7segHEX4  => HEX4,   -- dezenas de milhar
+        saida7segHEX5  => HEX5    -- centenas de milhar
+    );		  
+					  
+
+		--LEDR(0) <= CLK_1s;
+		
+		--LEDR(9 downto 6) <= entradaPC(3 downto 0);
 			
 		  
 end architecture;
--- arquivos_quartus/memoriaROM.vhd
+++ arquivos_quartus/memoriaROM.vhd
@@ -15,20 +15,68 @@ end entity;
 
 architecture assincrona of memoriaROM is
 
+
+
+	
+  constant NOP  : std_logic_vector(3 downto 0) 	:= "0000";
+  constant LDA  : std_logic_vector(3 downto 0) 	:= "0001";
+  constant LDI  : std_logic_vector(3 downto 0) 	:= "0010";
+  constant STA  : std_logic_vector(3 downto 0) 	:= "0011";
+  
+  constant SOMA : std_logic_vector(3 downto 0) 	:= "0100";
+  constant SOMAi : std_logic_vector(3 downto 0) := "0101";
+  
+  constant CLS : std_logic_vector(3 downto 0) 	:= "0110";
+  constant JLS : std_logic_vector(3 downto 0) 	:= "0111";
+  constant CEQ : std_logic_vector(3 downto 0) 	:= "1000";
+  constant JEQ : std_logic_vector(3 downto 0) 	:= "1001";
+  
+  constant JMP : std_logic_vector(3 downto 0) 	:= "1010";
+  
+  
+  
+  
+
   type blocoMemoria is array(0 TO 2**addrWidth - 1) of std_logic_vector(dataWidth-1 DOWNTO 0);
 
   function initMemory
         return blocoMemoria is variable tmp : blocoMemoria := (others => (others => '0'));
   begin
         -- Inicializa os endereços:
-        tmp(0) := "0000" & "000000000";
-        tmp(1) := "0001" & "000000001";
-        tmp(2) := "0010" & "000000010";
-        tmp(3) := "0011" & "000000011";
-        tmp(4) := "0000" & "000000100";
-        tmp(5) := "0001" & "000000101";
-        tmp(6) := "0010" & "000000110";
-        tmp(7) := "0011" & "000000111";
+        tmp(0) := LDI & "00000000" & "00000000"; -- 0
+        tmp(1) := STA & "00000000" & "00000000"; -- fib: 0 -> @0
+		  
+		  tmp(2) := LDI & "00000000" & "00000000"; -- 0
+        tmp(3) := STA & "00000000" & "00000001"; -- anterior: 0 -> @1
+		  
+        tmp(4) := LDI & "00000000" & "00000001"; -- 1
+        tmp(5) := STA & "00000000" & "00000010"; -- atual: 1 -> @2
+		  
+        tmp(6) := LDI & "00000000" & "00000000"; -- 0
+        tmp(7) := STA & "00000000" & "00000011"; -- prox: 0 -> @3
+		  
+        tmp(8) := LDI & "00000000" & "00001111"; -- 15
+        tmp(9) := STA & "00000000" & "00001010"; -- total: 15 -> @10
+		  
+		  
+		   
+		  
+		  tmp(10) := LDA & "00000000" & "00000001"; -- carrega anterior
+		  tmp(11) := SOMA & "00000000" & "00000010"; -- soma atual
+		  tmp(12) := STA & "00000000" & "00000011"; -- salva prox
+		  
+		  
+		  tmp(13) := LDA & "00000000" & "00000010"; -- carrega atual
+		  tmp(14) := STA & "00000000" & "00000001"; -- salva anterior
+		  
+		  tmp(15) := LDA & "00000000" & "00000011"; -- carrega prox
+		  tmp(16) := STA & "00000000" & "00000010"; -- salva atual
+        tmp(17) := STA & "00000000" & "00000000"; -- salva valor de fib
+		  tmp(18) := STA & "00000000" & "11111111"; -- printa no LED
+		  tmp(19) := JMP & "00000000" & "00001010"; -- pula para linha 10 (carrega atual)
+		  
+		  
+		  
         return tmp;
     end initMemory;
 
